45f3d7dda8b046fa9aa09deb3ed8c228544d9c53
schmeister <schmeister@users.noreply.github.com>
2024-04-26 15:43:20 +0200

    Update validate.go

--- validate/validate.go
+++ validate/validate.go
@@ -1,22 +1,39 @@
 package validate
 
 import (
+	"regexp"
 	"slices"
 
-	"github.com/schmeister/cve/internal/constants"
+	"gheprivate.intra.corp/vss/cots-management/internal/constants"
 )
 
-func ValidateFlags(	analysisStatePtr *string, analysisJustificationPtr *string) bool {
-
-	valid := true
-
-	if len(*analysisStatePtr) > 0 && !slices.Contains(constants.States, *analysisStatePtr) {
-		valid = false
+func ValidateFlags(flags constants.Flags) (bool, string) {
+	if len(flags.Component) == 0 {
+		return false, "Invalid component"
 	}
 
-	if len(*analysisJustificationPtr) > 0 && !slices.Contains(constants.Justifications, *analysisJustificationPtr) {
-		valid = false
+	if len(flags.State) == 0 || (len(flags.State) > 0 && !slices.Contains(constants.States, flags.State)) {
+		return false, "Invalid State"
 	}
 
-	return valid
+	if len(flags.Justification) > 0 && !slices.Contains(constants.Justifications, flags.Justification) {
+		return false, "Invalid Justification"
+	}
+
+	if len(flags.Details) < 10 && len(flags.Comment)<10 {
+		return false, "Invalid Details or Comments - either or both must have text of 10 characters longer"
+	}
+
+	return true, ""
+}
+
+func ValidateUUID(uuid string) (bool, string) {
+
+	uuidpattern := "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"
+	res1, e := regexp.MatchString(uuidpattern, uuid)
+	if !res1 || e != nil {
+		response := "Invalid Project UUID. Must match: " + uuidpattern
+		return false, response
+	}
+	return true, ""
 }
