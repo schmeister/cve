5971322f286f33f82039259c76c92dcf348fd21f
schmeister <schmeister@users.noreply.github.com>
2024-05-10 17:27:31 +0200

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -24,108 +24,106 @@ var PROJECT_ID = "882a7f3c-6791-4f84-9fba-d3aafa40c037" // GSS - ACP OS
 // var VULNERABILITY = "d93c63a4-7cea-4285-bbcc-767c0d7684fd" //CVE-2019-9924 (NVD)
 
 func main() {
-	sh := " (shorthand) "
 	var flags = constants.Flags{}
 
 	log.SetFlags(log.Lshortfile)
+	//log.SetFlags(log.Lmsgprefix)
 
-	flag.StringVar(&flags.Uri, "uri", URI, "URI of Dev-Track")
-	flag.StringVar(&flags.Uri, "U", URI, "URI of Dev-Track"+sh)
-
-	flag.StringVar(&flags.ApiKey, "apikey", API_KEY, "X-Api-Key")
-	flag.StringVar(&flags.ApiKey, "AK", API_KEY, "X-Api-Key"+sh)
-
-	flag.StringVar(&flags.Component, "component", "", "Component to be searched")
-	flag.StringVar(&flags.Component, "CP", "", "Component to be searched"+sh)
-
-	//flag.BoolVar(&flags.Suppressed, "suppressed", false, "Suppress the CVE")
-	//flag.BoolVar(&flags.Suppressed, "S", false, "Suppress the CVE"+sh)
-
-	flag.StringVar(&flags.State, "analysisState", "", "Analysis State: "+fmt.Sprint(constants.States))
-	flag.StringVar(&flags.State, "AS", "", "Analysis State: "+sh+fmt.Sprint(constants.States))
-
-	flag.StringVar(&flags.Justification, "analysisJustification", "", "Analysis Justification: "+fmt.Sprintln(constants.Justifications))
-	flag.StringVar(&flags.Justification, "AJ", "", "Analysis Justification: "+sh+fmt.Sprintln(constants.Justifications))
-
-	flag.StringVar(&flags.Project, "projectID", PROJECT_ID, "Project Details --> Object Identifier UUID")
-	flag.StringVar(&flags.Project, "PID", PROJECT_ID, "Project Details --> Object Identifier UUID"+sh)
-
-	flag.StringVar(&flags.Comment, "comment", "", "comments")
-	flag.StringVar(&flags.Comment, "COM", "", "comments"+sh)
-
-	flag.StringVar(&flags.Details, "analysisDetails", "", "Details")
-	flag.StringVar(&flags.Details, "DET", "", "Details"+sh)
-
-	flag.BoolVar(&flags.LP, "listP", false, "List projects")
-	flag.BoolVar(&flags.LP, "LP", false, "List projects"+sh)
-
-	flag.BoolVar(&flags.LC, "listC", false, "List components for project")
-	flag.BoolVar(&flags.LC, "LC", false, "List components for project"+sh)
-
-	flag.BoolVar(&flags.LV, "listV", false, "List vulnerabilities for project")
-	flag.BoolVar(&flags.LV, "LV", false, "List vulnerabilities for project"+sh)
-
-	flag.BoolVar(&flags.SIM, "simulate", false, "Simulate update only - If true only displays what will be updated")
-	flag.BoolVar(&flags.SIM, "SIM", false, "Simulate update only - If true only displays what will be updated"+sh)
-
-	flag.BoolVar(&flags.IS, "includesuppressed", false, "Include Supressed vulnerabilities - If false only non-suppressed vulnerabilities be included")
-	flag.BoolVar(&flags.IS, "IS", false, "Include Supressed vulnerabilities - If false only non-suppressed vulnerabilities be included"+sh)
-
+	flag.StringVar(&flags.Uri, "U", URI, "URI of Dev-Track")
+	flag.StringVar(&flags.ApiKey, "AK", API_KEY, "X-Api-Key")
+	flag.StringVar(&flags.Component, "CP", "", "Component to be searched")
+	flag.StringVar(&flags.State, "AS", "", "Analysis State: "+fmt.Sprint(constants.States))
+	flag.StringVar(&flags.Justification, "AJ", "", "Analysis Justification: "+fmt.Sprintln(constants.Justifications))
+	flag.StringVar(&flags.Project, "PID", PROJECT_ID, "Project Details --> Object Identifier UUID")
+	flag.StringVar(&flags.Comment, "COM", "", "comments")
+	flag.StringVar(&flags.Details, "DET", "", "Details")
+	flag.BoolVar(&flags.Help, "H", false, "Help")
+	flag.BoolVar(&flags.LP, "LP", false, "List projects")
+	flag.BoolVar(&flags.LC, "LC", false, "List components for project")
+	flag.BoolVar(&flags.LV, "LV", false, "List vulnerabilities for project")
+	flag.BoolVar(&flags.SIM, "SIM", false, "Simulate update only - If true only displays what will be updated")
+	flag.BoolVar(&flags.IS, "IS", false, "Include Supressed vulnerabilities - If false only non-suppressed vulnerabilities be included")
 	flag.BoolVar(&flags.REP, "REP", false, "Create report for Project")
-	flag.StringVar(&flags.FILE, "FILE", "", "Filename to save report to")
-
+	flag.StringVar(&flags.OUT, "OUT", "", "Output filename to save report to")
+	flag.StringVar(&flags.Vulnerability, "VUL", "", "Vulnerability name")
 	flag.Parse()
 
 	valid, vstr := validate.ValidateFlags(flags)
 
-	if flags.LP { // List Projects
-		projects := project.GetProjects(flags)
-		projects.ListProjects()
+	if flags.Help {
+		help.Help(flags)
+
+	} else if flags.LP { // List Projects
+		projs := project.GetProjects(flags)
+		projs.ListProjects(flags)
 	} else if flags.LC { // List Components
-		components := components.GetComponents(flags)
-		components.ListComponents()
-	} else if flags.REP { // Create report
+		if validate.IsValidUUID(flags.Project) {
+			components, cMap := components.GetProjectComponents(flags)
+			components.ListComponents(flags)
+			cMap.ListComponents(flags)
+		} else {
+			log.Printf("%s", help.Red("Invalid Project UUID format"))
+			return
+		}
+	} else if flags.LV { // List Vulnerabilities
+		if validate.IsValidUUID(flags.Project) {
+			_, acMap := components.GetProjectComponents(flags)
+			namedComps := acMap.GetComponentsFromName(flags)
+			cKeys := namedComps.Sort()
+
+			numVuls := 0
+
+			for _, key := range cKeys {
+				aComp := namedComps[key]
+
+				_, vuls := vulnerability.GetAllVulnerabilities(flags, "component", aComp.UUID)
+				vKeys := vuls.Sort()
+
+				for _, vKey := range vKeys {
+					vul := vuls[vKey]
+					wrapped2 := help.WordWrap(vul.Description, 80, 10)
+					fmt.Printf("%-25s %20s %s\n", help.Yellow(vul.VulnID), help.Magenta(aComp.Name+"-"+aComp.Version), help.Cyan(vul.Cwe.Name))
+					fmt.Printf("%s\n\n", help.White(wrapped2))
+				}
+				numVuls += len(vKeys)
+			}
+			log.Printf("Components     : %d", len(cKeys))
+			log.Printf("Vulnerabilities: %d", numVuls)
+		} else {
+			log.Printf("%s", help.Red("Invalid Project UUID format"))
+			return
+		}
+	} else if flags.REP { // Generate Report
 		an := reporting.Get(flags)
-		if len(flags.FILE) > 0 {
+		if len(flags.OUT) > 0 {
 			an.Save(flags)
 		} else {
 			an.Print()
 		}
-	} else if flags.LV { // List Vulnerabilities
-		v, res := validate.ValidateUUID(flags.Project)
-		if !v {
-			color.Set(color.FgYellow)
-			log.Println(res)
-			color.Unset()
-		} else {
-			vulnerabilities := vulnerability.GetVulnerabilities(flags, "project", flags.Project, 1)
-			vulnerabilities.ListVulnerabilities()
-			log.Println(len(vulnerabilities))
-		}
 	} else if !valid { // if not valid so far, print help.
-		color.Set(color.FgYellow)
-		log.Println(vstr)
-		color.Unset()
+		log.Printf("%s", help.Red(vstr))
 
 		flag.PrintDefaults()
 	} else {
-		comps := components.GetComponents(flags)
-		uuids := components.GetComponent(flags, comps)
-		log.Printf("# Components: %d %s\n", len(uuids), uuids)
-		for _, uuid := range uuids {
-			page := 1
-			vuls := vulnerability.GetAllVulnerabilities(flags, "component", uuid)
-			for idx2, y := range vuls {
+		allComps, _ := components.GetProjectComponents(flags)
+		namedComps := allComps.GetComponentsFromName(flags)
+		proj := project.GetProject(flags)
+		log.Printf("# Components: %d\n", len(namedComps))
+		count := 1
+		for _, aComp := range namedComps {
+			_, vuls := vulnerability.GetAllVulnerabilities(flags, "component", aComp.UUID)
+			keys := vuls.Sort()
+			for _, key := range keys {
+				vul := vuls[key]
 				if flags.State == "NOT_SET" || flags.State == "IN_TRIAGE" {
 					flags.Suppressed = false
 				} else {
 					flags.Suppressed = true
 				}
-
 				an := analysis.PutAnalysis{
 					Project:               flags.Project,
-					Component:             uuid,
-					Vulnerability:         y.UUID,
+					Component:             aComp.UUID,
+					Vulnerability:         vul.UUID,
 					AnalysisState:         flags.State,
 					AnalysisJustification: flags.Justification,
 					AnalysisResponse:      "CAN_NOT_FIX",
@@ -135,12 +133,12 @@ func main() {
 					AnalysisDetails:       flags.Details,
 				}
 				if !flags.SIM {
-					analysis.Save(flags, an)
+					an.Save(flags)
 				}
-				num := ((page - 1) * 100) + idx2 + 1
-				log.Printf("Vul: %4d %s %s %s %s \"%s\" \"%s\" \n", num, an.Project, an.Component, an.Vulnerability, an.AnalysisState, an.Comment, an.AnalysisDetails)
+				log.Printf("%4d [%s] [%s-%s] [%-14s] %s %s \"%s\" \n", count, proj.Name, aComp.Name, aComp.Version, vul.VulnID, an.AnalysisState, an.AnalysisJustification, an.AnalysisDetails)
+				count++
 			}
-			log.Println(len(vuls))
 		}
+		project.Refresh(flags)
 	}
 }
