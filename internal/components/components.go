30fd086ee0e59d6432c832b88dbef3ab818abe41
schmeister <schmeister@users.noreply.github.com>
2024-05-10 17:30:46 +0200

    Update components.go

--- internal/components/components.go
+++ internal/components/components.go
@@ -10,8 +10,10 @@ import (
 
 	"github.com/schmeister/cve/internal/constants"
 )
+package components
 
 type Components []Component
+type CMap map[string]Component
 
 type Component struct {
 	Name               string `json:"name"`
@@ -75,19 +77,18 @@ type Component struct {
 	Cpe     string `json:"cpe,omitempty"`
 }
 
-func GetComponents(flags constants.Flags) Components {
+func GetProjectComponents(flags constants.Flags) (Components, CMap) {
 	page := 1
-	pageSize := 100
+	pageSize := 200
 	components := Components{}
 	hasMore := true
 
 	for hasMore {
-		pageNum := fmt.Sprint(page)
-		pageS := fmt.Sprintf("%d", pageSize)
 
-		url := flags.Uri + "/api/v1/component/project/" +
-			flags.Project + "?onlyOutdated=false&onlyDirect=false" +
-			"&pageNumber=" + pageNum + "&pageSize=" + pageS
+		url := flags.Uri + "/api/v1/component/project/" + flags.Project +
+			"?onlyOutdated=false&onlyDirect=false" +
+			"&pageNumber=" + fmt.Sprintf("%d", page) +
+			"&pageSize=" + fmt.Sprintf("%d", pageSize)
 
 		req, err := http.NewRequest(
 			http.MethodGet,
@@ -116,26 +117,118 @@ func GetComponents(flags constants.Flags) Components {
 		components = append(components, comps...)
 
 		if len(comps) < pageSize {
-			return components
+			break
 		}
 		page++
 	}
-	return components
+
+	cMap := make(CMap)
+	for _, y := range components {
+		cMap[y.Name+y.Version] = y
+	}
+
+	return components, cMap
 }
 
-func GetComponent(flags constants.Flags, components Components) []string {
-	uuids := make([]string, 0)
-	for _, y := range components {
-		if strings.EqualFold(y.Name, flags.Component) {
-			uuids = append(uuids, y.UUID)
+func GetComponentFromUUID(flags constants.Flags) Component {
+	component := Component{}
+	hasMore := true
+
+	for hasMore {
+
+		url := flags.Uri + "/api/v1/component/" + flags.Component
+
+		req, err := http.NewRequest(
+			http.MethodGet,
+			url,
+			nil,
+		)
+		if err != nil {
+			log.Fatalf("error creating HTTP request: %v", err)
+		}
+
+		req.Header.Add("Accept", "application/json")
+		req.Header.Add("X-Api-Key", flags.ApiKey)
+
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Fatalf("error sending HTTP request: %v", err)
+		}
+
+		responseBytes, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Fatalf("error reading HTTP response body: %v", err)
+		}
+
+		json.Unmarshal(responseBytes, &component)
+	}
+	return component
+}
+
+func (components Components) GetComponentsFromName(flags constants.Flags) Components {
+	if flags.Component == "" {
+		return components
+	}
+
+	comps := make(Components, 0)
+
+	for _, comp := range components {
+		if strings.EqualFold(comp.Name, flags.Component) {
+			comps = append(comps, comp)
 		}
 	}
-	return uuids
+	return comps
 }
 
-func (components Components) ListComponents() {
-	for _, component := range components {
-		log.Printf("%-25s %s\n", component.Name, component.UUID)
+func (components CMap) GetComponentsFromName(flags constants.Flags) CMap {
+	if flags.Component == "" {
+		return components
 	}
-	log.Println(len(components))
+
+	comps := make(CMap, 0)
+
+	for _, comp := range components {
+		if strings.EqualFold(comp.Name, flags.Component) {
+			comps[comp.Name] = comp
+		}
+	}
+	return comps
+}
+
+func (components Components) ListComponents(flags constants.Flags) {
+	yellow := color.New(color.FgYellow).SprintFunc()
+	red := color.New(color.FgHiRed).SprintFunc()
+	blue := color.New(color.FgHiBlue).SprintFunc()
+
+	projects := project.GetProjects(flags)
+	project := projects.GetProjectFromUUID(flags.Project)
+
+	log.Printf("%-25s%20s(%s)\n", red(project.Name), "", project.UUID)
+	for _, component := range components {
+		log.Printf("%-25s%-20s(%s)\n", yellow(component.Name), blue(component.Version), component.UUID)
+	}
+}
+
+func (cmap CMap) ListComponents(flags constants.Flags) {
+	yellow := color.New(color.FgYellow).SprintFunc()
+	red := color.New(color.FgHiRed).SprintFunc()
+	blue := color.New(color.FgHiBlue).SprintFunc()
+
+	projects := project.GetProjects(flags)
+	project := projects.GetProjectFromUUID(flags.Project)
+
+	log.Printf("%-25s%20s(%s)\n", red(project.Name), "", project.UUID)
+	for _, component := range cmap {
+		log.Printf("%-25s%-20s(%s)\n", yellow(component.Name), blue(component.Version), component.UUID)
+	}
+}
+
+func (vuls CMap) Sort() []string {
+
+	keys := make([]string, 0, len(vuls))
+	for k := range vuls {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
 }
