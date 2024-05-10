0b276005034cbe594a221c15db6281b42395443f
schmeister <schmeister@users.noreply.github.com>
2024-05-10 18:19:37 +0200

    Update validate.go

--- validate/validate.go
+++ validate/validate.go
@@ -1,10 +1,10 @@
 package validate
 
 import (
-	"regexp"
 	"slices"
+	"github.com/google/uuid"
 
-	"github.com/schmeister/cve/internal/constants"
+	"gheprivate.intra.corp/vss/cots-management/internal/constants"
 )
 
 func ValidateFlags(flags constants.Flags) (bool, string) {
@@ -20,20 +20,14 @@ func ValidateFlags(flags constants.Flags) (bool, string) {
 		return false, "Invalid Justification"
 	}
 
-	if len(flags.Details) < 10 && len(flags.Comment) < 10 {
+	if len(flags.Details) < 10 && len(flags.Comment)<10 {
 		return false, "Invalid Details or Comments - either or both must have text of 10 characters longer"
 	}
 
 	return true, ""
 }
 
-func ValidateUUID(uuid string) (bool, string) {
-
-	uuidpattern := "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"
-	res1, e := regexp.MatchString(uuidpattern, uuid)
-	if !res1 || e != nil {
-		response := "Invalid Project UUID. Must match: " + uuidpattern
-		return false, response
-	}
-	return true, ""
-}
+func IsValidUUID(u string) bool {
+    _, err := uuid.Parse(u)
+    return err == nil
+ }
